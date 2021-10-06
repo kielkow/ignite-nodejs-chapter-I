df680aced4e4ed1ee4be47929c6caab824a77e93
kielkow <matheuskiel@fiorifer.com.br>
2021-10-05 23:53:32 -0300

    feat: add http methods

--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
-
-app.get('/', (request, response) => {
-    return response.json({
-        message: "Hello World Ignite!"
-    })
-})
-
+const app = express();
+app.use(express.json())
 app.listen(3333);
+
+let courses = [];
+let globalId = 0;
+
+app.get('/courses', (request, response) => {
+    return response.json(courses);
+});
+
+app.post('/courses', (request, response) => {
+    globalId += 1;
+    courses.push({ id: globalId, ...request.body});
+    return response.json(courses);
+});
+
+app.put('/courses/:id', (request, response) => {
+    courses.map((course, index, _) => {
+        if (course.id == request.params.id) {
+            courses[index] = { id: course.id, ...request.body };
+        }
+    });
+    return response.json(courses);
+});
+
+app.delete('/courses/:id', (request, response) => {
+    courses = courses.filter(course => course.id != request.params.id);
+    return response.json(courses);
+});
